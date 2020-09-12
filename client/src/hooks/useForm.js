1f9b60b3041c79b4f939e0e453ba7969b8c8db2d
Kyla Oyamot <koyamot@gmail.com>
2020-09-11 19:19:54 -0700

    plant list displayed, useForm hook created

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValue) => {
+    const [values, setValues] = useState()
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+    return [values, handleChange]
+}
+
+export default useForm;
